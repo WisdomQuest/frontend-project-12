ff1f28d881fddde35d459d62884f0c34fe826bc5
Pavel Sofronov <sofronovpv89@mail.ru>
2025-10-27 12:01:03 +0400

    temp rename to change case

--- frontend/src/modules/registration/registration.jsx
+++ frontend/src/modules/registration/registration.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { FormRegister } from './formRegister.jsx'
+import { FormRegister } from './formReg.jsx'
 import registerLogo from '../../assets/images/registerLogo.jpg'
 import { CircleAvater } from '../../common/circleAvater.jsx'
 
